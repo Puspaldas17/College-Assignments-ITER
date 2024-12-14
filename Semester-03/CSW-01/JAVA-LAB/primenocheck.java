697a8dd3e632b1c11d02d77569ac194ab38ab3dd
Puspal Das <147488708+Puspaldas17@users.noreply.github.com>
2024-12-14 16:32:09 +0530

    Update primenocheck.java

--- Semester-03/CSW-01/JAVA-LAB/primenocheck.java
+++ Semester-03/CSW-01/JAVA-LAB/primenocheck.java
@@ -1,17 +1,31 @@
-import java.util.*;
-public class primenocheck {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number");
-        int n = sc.nextInt();
-        boolean flag = true;
-        for (int i = 2; i <= n / 2; i++) {
-            if (n % i == 0) {
-                flag = false;
-                break;
-            }
+import java.util.Scanner;
 
-    
-}
+public class PrimeCheck {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number to check if it's prime: ");
+        int number = scanner.nextInt();
+
+        int isPrime = 1; 
+
+        if (number < 2) {
+            isPrime = 0;
+        } else {
+            for (int i = 2; i * i <= number; i++) { 
+                if (number % i == 0) {
+                    isPrime = 0;
+                    break;
+                }
+            }
+        }
+
+        if (isPrime == 1) {
+            System.out.println(number + " is a prime number.");
+        } else {
+            System.out.println(number + " is not a prime number.");
+        }
+
+        scanner.close();
     }
 }
